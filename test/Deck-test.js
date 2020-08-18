6395a41d6aae6daad24cb5ba1f38e3337feeeee9
Nathan Darrington <npdarrington@icloud.com>
2020-08-17 20:31:49 -0600

    Added test for Deck to instantiate with an array of cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -14,4 +14,12 @@ describe('Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
+
+  it('should instantiate with an array of cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cardSet).to.deep.equal([card1, card2, card3]);
+  });
 });
