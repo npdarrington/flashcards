2e6b9b830b506f36936514897d98b72bb88a2a1a
Nathan Darrington <npdarrington@icloud.com>
2020-08-17 20:08:50 -0600

    Added test for Deck to be a function

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,3 +3,10 @@ const expect = chai.expect;
 
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+
+describe('Deck', function() {
+  it('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+});
