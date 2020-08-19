a9f1dbacabe169a7e8724d4516c51d2e11e4ba9c
Nathan Darrington <npdarrington@icloud.com>
2020-08-18 18:55:47 -0600

    Added test for Round to play a turn when a guess is made

--- test/Round-test.js
+++ test/Round-test.js
@@ -59,4 +59,15 @@ describe('Round', () => {
     const round = new Round(deck);
     expect(round.takeTurn).to.be.a('function');
   });
+
+  it('should play a turn when a guess is made', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    const playRound = round.takeTurn('pug');
+    expect(playRound).to.be.an.instanceof(Turn);
+    expect(round.currentCard).to.equal(card1);
+  });
 });
