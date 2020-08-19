5954cf1c0df31704f730b074b929622d4ba9d137
Nathan Darrington <npdarrington@icloud.com>
2020-08-18 18:34:40 -0600

    Added test for Round to instantiate with a Deck of cards

--- test/Round-test.js
+++ test/Round-test.js
@@ -16,4 +16,13 @@ describe('Round', () => {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   });
+
+  it('should instantiate with a deck of cards', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck).to.deep.equal(deck);
+  });
 });
