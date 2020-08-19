39a66ab47e7d994fc7a37f29d1aca63104969531
Nathan Darrington <npdarrington@icloud.com>
2020-08-18 18:29:56 -0600

    Added test for Round to be a function

--- test/Round-test.js
+++ test/Round-test.js
@@ -6,3 +6,9 @@ const Card = require('../src/Card');
 const Deck = require('..src/Deck');
 const Round = require('../src/Round.js');
 
+describe('Round', () => {
+  it('should be a function', () => {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+});
