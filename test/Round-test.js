c62ac666d99d491efd83aa3c2411de37e8eaffc7
Nathan Darrington <npdarrington@icloud.com>
2020-08-18 20:22:11 -0600

    Added test for Round to store the id of cards that have been guessed incorrectly

--- test/Round-test.js
+++ test/Round-test.js
@@ -103,4 +103,18 @@ describe('Round', () => {
     evaluateGuess = round.takeTurn('appendix');
     expect(evaluateGuess).to.equal('incorrect!');
   });
+
+  it('should store the id of the cards on incorrect guesses', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    let evaluateGuess = round.takeTurn('sea otter');
+    expect(evaluateGuess).to.equal('correct!');
+    expect(round.incorrectGuesses).to.deep.equal([]);
+    evaluateGuess = round.takeTurn('appendix');
+    expect(evaluateGuess).to.equal('incorrect!');
+    expect(round.incorrectGuesses).to.deep.equal([2]);
+  });
 });
