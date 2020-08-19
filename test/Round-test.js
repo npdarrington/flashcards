f844348790af4b9e73fa9c60ab7c79a5e67834ce
Nathan Darrington <npdarrington@icloud.com>
2020-08-18 18:58:57 -0600

    Added test for Round to update turn count each time a turn is played

--- test/Round-test.js
+++ test/Round-test.js
@@ -70,4 +70,14 @@ describe('Round', () => {
     expect(playRound).to.be.an.instanceof(Turn);
     expect(round.currentCard).to.equal(card1);
   });
+
+  it('should update the round each time a turn is played regardless of correct or incorrect answer', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    round.takeTurn('capybara');
+    expect(round.turns).to.equal(1);
+  });
 });
