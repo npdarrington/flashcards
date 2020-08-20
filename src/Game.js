c90b29c3c3a63c143863992c8f3033df38d41836
Nathan Darrington <npdarrington@icloud.com>
2020-08-19 23:07:01 -0600

    Added require files needed for all classes to interact with Game

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,17 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Turn = require('../src/Test');
+
 class Game {
   constructor() {}
+
+  start() {
+    
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
