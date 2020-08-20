31472142fbd0ac3d7fa2c9d67da849009375aa02
Nathan Darrington <npdarrington@icloud.com>
2020-08-19 23:01:32 -0600

    Added tests for Game to be a function and instance of Game

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,8 +1,15 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
-const Round = require('../src/Round');
-const Turn = require('../src/Test');
+const Game = require('../src/Game');
 
+describe('Game', () => {
+  it('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', () => {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  });
+});
